ad796dc1f955afce60e07700d12f26a4275d77f4
yuliu <liu@lius-MBP.lan>
2019-12-13 13:26:28 +0800

    feat: unified function interface of python script

--- python/command.py
+++ python/command.py
@@ -1,6 +1,7 @@
 import sys
+import os
 import argparse
-from process_file import processFile, processFileStream
+from process_file import processFileStream
 
 parser = argparse.ArgumentParser(
     description='Process some input source', epilog="UPPERCASE YOUR HEAD 😇  😇",
@@ -14,7 +15,11 @@ group.add_argument("--input", "-", default=True,
 args = parser.parse_args()
 
 
+BASE_PATH = os.getenv("BASE_PATH") or os.path.dirname(__file__)
+
 if(args.file):
-  processFile(args.file)
+  abs_file_path = os.path.realpath(os.path.join(BASE_PATH, args.file))
+  with open(abs_file_path, 'r') as in_file:
+    processFileStream(in_file.readline)
 elif (args.input):
   processFileStream(sys.stdin)
--- python/process_file.py
+++ python/process_file.py
@@ -5,16 +5,11 @@ BASE_PATH = os.getenv("BASE_PATH") or os.path.dirname(__file__)
 OUT_PATH = os.path.join(BASE_PATH, "out.txt")
 
 
-def processFile(file_path):
-  print(11, file_path)
-  abs_file_path = os.path.realpath(os.path.join(BASE_PATH, file_path))
-  with open(abs_file_path, 'r') as in_file, open(OUT_PATH, 'w') as out_file:
-    for line in iter(in_file.readline, ''):
-      out_file.write(line.upper())
-
-
-def processFileStream(stream):
+def processFileStream(obj):
   with open(OUT_PATH, 'w') as out_file:
-    for line in iter(stream):
+    # docs, if second argument present, the first must be callable
+    iter_obj = iter(obj) if not callable(obj) else iter(obj, "")
+    for line in iter_obj:
       out_file.write(line.upper())
-      print("successfully converted to uppercase")
+
+  print("successfully converted to uppercase")
